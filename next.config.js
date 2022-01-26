60085d33c2e7cc4fdd82b073309d4c663bf42d86
youthfulhps <ybh942002@gmail.com>
2022-01-26 09:52:24 +0900

    Implement image ondemand resizing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      'd1uuq5rh6ftjrd.cloudfront.net'
+    ]
+  }
 }
 
 module.exports = nextConfig
